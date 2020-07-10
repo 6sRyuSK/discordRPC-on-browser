6c18262842bbdb55e464274b873ffdd8c4b81e40
6sRyuSK <zikkyou19@gmail.com>
2020-07-10 12:38:18 +0900

    add discordOAuth.ts

---
+++ api/discordOAuth.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import querystring from 'querystring'
+import fetch from 'node-fetch'
+const { DiscordClientID, DiscordSecret, DiscordRedirectURI } = process.env
+
+export const handler = async(event) => {
+  if(event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
+  const params = querystring.parse(event.body)
+  if(!params.code) {
+    return { statusCode: 400, body: 'code is null' }
+  }
+  const code = params.code
+
+  const body = {
+    client_id: DiscordClientID,
+    client_scret: DiscordSecret,
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: DiscordRedirectURI,
+    scope: 'rpc',
+  }
+
+  return fetch('https://discord.com/api/oauth2/token', {
+    method: 'POST',
+    header: { 'Content-Type': 'application/json; charset=utf-8' },
+    body: JSON.stringify(body),
+  }).then(result => result.json())
+}
